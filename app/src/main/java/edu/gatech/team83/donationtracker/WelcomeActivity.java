fb35392b5f5e3386c702d244069bceef9d7e66ea
sdoss7 <tenguinpoikmn09>
2018-09-19 12:09:16 -0400

    Starter files uploaded

---
+++ app/src/main/java/edu/gatech/team83/donationtracker/WelcomeActivity.java
@@ -0,0 +1,13 @@
+package edu.gatech.team83.donationtracker;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class WelcomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcome);
+    }
+}
