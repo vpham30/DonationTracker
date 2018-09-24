5a3e050d08cd448d49c9d88a320a87d21ec02df6
skapur12 <42812319+skapur12@users.noreply.github.com>
2018-09-24 11:41:24 -0400

    Update WelcomeActivity.java

--- app/src/main/java/edu/gatech/team83/donationtracker/WelcomeActivity.java
+++ app/src/main/java/edu/gatech/team83/donationtracker/WelcomeActivity.java
@@ -3,6 +3,10 @@ package edu.gatech.team83.donationtracker;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import android.content.Intent;
+import android.view.View;
+import android.widget.Button;
+
 public class WelcomeActivity extends AppCompatActivity {
 
     @Override
