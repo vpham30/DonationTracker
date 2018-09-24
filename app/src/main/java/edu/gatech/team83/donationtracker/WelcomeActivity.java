fd04d72cd735b43c8612c5cbd40b0a8b91dfe748
skapur12 <42812319+skapur12@users.noreply.github.com>
2018-09-24 05:27:51 -0400

    Update WelcomeActivity.java

--- app/src/main/java/edu/gatech/team83/donationtracker/WelcomeActivity.java
+++ app/src/main/java/edu/gatech/team83/donationtracker/WelcomeActivity.java
@@ -10,4 +10,43 @@ public class WelcomeActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
     }
+    
+    login = (Button) findViewById(R.id.login);
+    register = (Button) findViewById(R.id.register);
+    back = (Button) findViewById(R.id.back);
+    
+    login.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            onLoginPressed(v);
+        }
+    });
+    
+    register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onRegisterPressed(v);
+            }
+    });
+    
+    back.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            onBackPressed(v);
+        });
+        
+        public void onLoginPressed(View view) {
+        Intent intent = new Intent(this, LoginActivity.class);
+        startActivity(intent);
+    }
+        
+        public void onRegisterPressed(View view) {
+            Intent intent = new Intent(this, LoginActivity.class);
+            startActivity(intent);
+        }
+
+        public void onBackPressed(View view) {
+            Intent intent = new Intent(this, WelcomeActivity.class);
+            startActivity(intent);
+        }
 }
