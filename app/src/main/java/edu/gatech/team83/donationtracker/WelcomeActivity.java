5e54efa7f3869ba2d065428d7c0d9f64912e0866
Victoria Pham <42808255+vpham30@users.noreply.github.com>
2018-09-27 10:37:54 -0400

    Add three text fields for username, password, and confirm password. Still missing two buttons for register and cancel

--- app/src/main/java/edu/gatech/team83/donationtracker/WelcomeActivity.java
+++ app/src/main/java/edu/gatech/team83/donationtracker/WelcomeActivity.java
@@ -10,13 +10,26 @@ import android.view.View;
 import android.widget.Button;
 
 public class WelcomeActivity extends AppCompatActivity {
+    private Button register;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
+
+        register = (Button) findViewById(R.id.register);
+        register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openRegistration();
+            }
+        });
     }
 
+    public void openRegistration() {
+        Intent intent = new Intent (this, Registration.class);
+        startActivity(intent);
+    }
         
     public void onLoginPressed(View view) {
         Intent intent = new Intent(this, LoginActivity.class);
