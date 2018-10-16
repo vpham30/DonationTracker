e313b905d8b8812a3880e361680d3290e68abee3
sdoss7 <sdoss7@gatech.edu>
2018-10-16 14:29:10 -0400

    My Fix broke it sorry

--- app/src/main/java/edu/gatech/team83/donationtracker/controller/LocationDetailActivity.java
+++ app/src/main/java/edu/gatech/team83/donationtracker/controller/LocationDetailActivity.java
@@ -49,7 +49,7 @@ public class LocationDetailActivity extends AppCompatActivity {
         startActivity(intent);
     }
 
-    public void onLocationSubmitPressed(View v) {
+    public void onEditLocationPressed(View v) {
         Context context = v.getContext();
         Intent intent = new Intent(context, LocationEditActivity.class);
         intent.putExtra("Location", location);
