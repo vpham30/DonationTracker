8d9075d23001e67682601788f79f7de4079692c2
Victoria Pham <42808255+vpham30@users.noreply.github.com>
2018-10-16 09:51:59 -0400

    Locational Detail Activity

---
+++ app/src/main/java/edu/gatech/team83/donationtracker/controller/LocationDetailActivity.java
@@ -0,0 +1,43 @@
+package edu.gatech.team83.donationtracker.controller;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.TextView;
+
+import edu.gatech.team83.donationtracker.R;
+import edu.gatech.team83.donationtracker.model.Location;
+import edu.gatech.team83.donationtracker.model.Model;
+
+public class LocationDetailActivity extends AppCompatActivity {
+    private Model location;
+    private TextView locationName;
+    private TextView locationType;
+    private TextView longitude;
+    private TextView latitude;
+    private TextView address;
+    private TextView phone;
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.location_detail);
+
+        // TODO need to access location
+
+
+    }
+
+    public void onBackPressed(View v) {
+        Context context = v.getContext();
+        Intent intent = new Intent(context, LocationRecyclerActivity.class);
+        startActivity(intent);
+    }
+
+    public void onEditLocationPressed(View v) {
+        Context context = v.getContext();
+        Intent intent = new Intent(context, LocationEditActivity.class);
+        startActivity(intent);
+    }
+}
