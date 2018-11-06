b5e1d885d2f5c47b5fa229c6ce3fd78c863393dc
Victoria Pham <42808255+vpham30@users.noreply.github.com>
2018-11-06 10:55:48 -0500

    Update LocationEditActivity.java

--- app/src/main/java/edu/gatech/team83/donationtracker/controller/LocationEditActivity.java
+++ app/src/main/java/edu/gatech/team83/donationtracker/controller/LocationEditActivity.java
@@ -6,7 +6,6 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
-import android.widget.Toolbar;
 
 
 import edu.gatech.team83.donationtracker.R;
