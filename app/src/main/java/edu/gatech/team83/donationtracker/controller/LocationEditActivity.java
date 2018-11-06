0b0350ea1267b6b7d355b4b62ab0a5316d878a84
Victoria Pham <42808255+vpham30@users.noreply.github.com>
2018-11-06 10:53:57 -0500

    Changed screen titles. (i.e. LoginActivity -> Login)

--- app/src/main/java/edu/gatech/team83/donationtracker/controller/LocationEditActivity.java
+++ app/src/main/java/edu/gatech/team83/donationtracker/controller/LocationEditActivity.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Toolbar;
 
 
 import edu.gatech.team83.donationtracker.R;
